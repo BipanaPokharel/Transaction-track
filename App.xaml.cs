cb7a9c9eca7a1239dfa05b2355805f310cd0671b
aalukhau <bpnapkhrl4@gmail.com>
2024-12-17 19:22:44 +0545

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Project MAUI" };
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
